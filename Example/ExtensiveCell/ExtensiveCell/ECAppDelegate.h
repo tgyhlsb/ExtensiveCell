2d0a5c6703d57430a692cfbe29dcdeb78fadaec0
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2013-11-02 13:26:34 +0100

    XCode new project

---
+++ Example/ExtensiveCell/ExtensiveCell/ECAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ECAppDelegate.h
+//  ExtensiveCell
+//
+//  Created by Tanguy Hélesbeux on 02/11/2013.
+//  Copyright (c) 2013 Tanguy Hélesbeux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ECAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/ExtensiveCell/ExtensiveCell/ECViewController.h
@@ -0,0 +1,13 @@
+//
+//  ECViewController.h
+//  ExtensiveCell
+//
+//  Created by Tanguy Hélesbeux on 02/11/2013.
+//  Copyright (c) 2013 Tanguy Hélesbeux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ECViewController : UIViewController
+
+@end

bbdcbe3bdefc82728be310d67c3dcfdf647fa88c
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2013-11-25 20:09:17 +0100

    comments fail

--- ExtensiveCell/ECViewController.h
+++ ExtensiveCell/ECViewController.h
@@ -31,7 +31,7 @@
 
 /*****
  
- Asks the data source for a view to fill in a the container when selecting a row.
+ Asks the data source for a view to display in the container when selecting a row.
  You should fetch a previously created view object for performance reasons.
  
  The ECViewcontroller tableview only has one container and reuses it.
@@ -56,7 +56,7 @@
 
 /*****
  
- Similar to numberOfSectionsInTableView: frm UITableViewDataSource.
+ Similar to numberOfSectionsInTableView: from UITableViewDataSource.
  
  *****/
 - (NSInteger)numberOfSections;
@@ -64,7 +64,7 @@
 
 /*****
  
- Similar to tableview:numberOfRowsInSection: frm UITableViewDataSource.
+ Similar to tableview:numberOfRowsInSection: from UITableViewDataSource.
  
  Do not consider the container (open or closed) when calculating the number of rows.
  
